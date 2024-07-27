4caf9a00ec888531629b1566df543f65e8d59d8e
Karan Kadam <karank@outlook.com>
2024-07-27 11:58:49 +0930

    feat: 🎸 add basic code to read rtc and activate servo

--- buttonpusher/src/main.cpp
+++ buttonpusher/src/main.cpp
@@ -1,18 +1,35 @@
 #include <Arduino.h>
+#include <Wire.h>  // for I2C communication with RTC module
+#include <RTClib.h>  // for date and time handling
 
-// put function declarations here:
-int myFunction(int, int);
+RTC_DS3231 rtc;  // define the RTC object
+time_t time_t_values[] = {Time(07, 30, 0), Time(09, 30, 0), Time(12,0,0), Time(14,0,0)};  // example time values
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  if (!rtc.begin()) {
+    Serial.println("RTC not connected!");
+  } else {
+    Serial.println("RTC connected!");
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  Time now = rtc.now();
+
+  for (int i = 0; i < sizeof(time_t_values) / sizeof(time_t); i++) {
+    Time compareTime = time_t_values[i];
+    if (now == compareTime) {
+      Serial.println("Matched time!");
+      // Activate servo and move it from 0-180 degrees
+      analogWrite(SERVO_PIN, 0);  // initialize servo to 0 degrees
+      delay(2000);
+      for (int deg = 1; deg <= 180; deg++) {
+        analogWrite(SERVO_PIN, map(deg, 0, 180, 0, 255));
+        delay(10);
+      }
+      break;
+    }
+  }
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
